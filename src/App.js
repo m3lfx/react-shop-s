987599bc65864db31e0a1dc753459df141c46ed4
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-13 09:04:20 +0800

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './Components/Layout/Header'
 import Footer from './Components/Layout/Footer'
-
+import Home from './Components/Home';
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
